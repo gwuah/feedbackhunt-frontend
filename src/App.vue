c56d586a61ef4e270724b6e7a99e90b9442c48f8
gwuah <griffith@dolphtech.com>
2018-07-24 16:07:47 +0000

    main component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header/>
+    <Body/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+  
+  import Header from '@/components/Header'
+  import Body from '@/components/Body'
 
+  export default {
+    name: "App",
+    components: {
+      Header,
+      Body
+    }
+  }
+</script>
+
+
+<style>
 #nav a {
   font-weight: bold;
   color: #2c3e50;
